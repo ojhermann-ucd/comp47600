1851b5f4ec33d01bfad1ce516e14dc29efeca395
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-10 12:46:42 +0100

    q1b augmented done

--- practical4/q1/b.py
+++ practical4/q1/b.py
@@ -40,15 +40,27 @@ def tf_log_scaled(token_list, base):
 	"""
 	This function turns the boolean frequncy and transforms it into a log-scaled frequency
 	"""
-	log_scaled_outputs = tf_boolean_dict(token_list)
-	for token in log_scaled_outputs[1]:
-		log_scaled_outputs[1][token] = round(math.log(log_scaled_outputs[1][token], base),2)
-	return log_scaled_outputs
+	log_scaled_dict = tf_boolean_dict(token_list)[0]
+	for token in log_scaled_dict:
+		log_scaled_dict[token] = round(math.log(log_scaled_dict[token], base),2)
+	return log_scaled_dict
 
 
+
+def max_value_in_dict(this_dict):
+	maximum_value = 0
+	for key in this_dict:
+		maximum_value = max(maximum_value, this_dict[key])
+	return maximum_value
 
 # Augmented frequency
-
+def tf_augmented_frequency(token_list):
+	boolean_outputs = tf_boolean_dict(token_list) 
+	augmented_dict = tf_boolean_dict(token_list)[0]
+	maximum_token_count = max_value_in_dict(boolean_outputs[0])
+	for token in augmented_dict:
+		augmented_dict[token] = 0.5 + (0.5 * augmented_dict[token]) / maximum_token_count
+	return augmented_dict
 
 
 
@@ -60,17 +72,22 @@ if __name__ == '__main__':
 		# generate the data
 		token_list = a.tokenize_text_file_remove_stop_words(file_name)
 		boolean_outputs = tf_boolean_dict(token_list)
-		log_scaled_outputs = tf_log_scaled(token_list, 2)
+		log_scaled_dict = tf_log_scaled(token_list, 2)
+		augmented_dict = tf_augmented_frequency(token_list)
 
 		# general information
 		print("")
+		print("")
 		print("File: {}".format(file_name)) # print the file name
+		print("")
 		print("Token List: {}".format(token_list))
-
+		print("")
 		# boolean
 		print("TF Boolean Frequencies: {}".format(boolean_outputs[1]))
-
+		print("")
 		# log scaled
-		print("TF Log-Scaled Frequencies: {}".format(log_scaled_outputs[1]))
-
+		print("TF Log-Scaled Frequencies: {}".format(log_scaled_dict))
+		print("")
 		# augmented frequncy
+		print("TF Augmented Frequencies: {}".format(augmented_dict))
+		print("")
