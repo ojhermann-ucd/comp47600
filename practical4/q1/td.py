b9e1ef8126ffb370836e2d73031eb83641fd080f
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-10 10:30:20 +0100

    sourced files

--- practical4/q1/td.py
+++ practical4/q1/td.py
@@ -3,19 +3,15 @@
 
 
 
-def tf_boolean(term_dict, text):
-	for word in text:
-		if word in term_dict:
-			term_dict[word] += 1
+def tf_boolean_dict(term_dict, text):
+	"""
+	Inputs
+	- term_dict: key: item from text, value: instantiated at 0
+	- text: string of the text t
+	"""
+	for item in text:
+		if item in term_dict:
+			term_dict[item] += 1
 	return term_dict
 
-
-
-def tf_log_scalred(term_dict, text, base):
-	log_dict = tf_boolean(term_dict, text)
-	for word in log_dict:
-		log_dict[word] = log(log_dict[word], base) + 1
-	return log_dict
-
-
-
+def tf_boolean(term_dict)
