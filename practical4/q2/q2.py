4bfd4634d850d55bd219d7c86eb98e343f38b45c
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-12 19:38:17 +0100

    q2 done

--- practical4/q2/q2.py
+++ practical4/q2/q2.py
@@ -100,6 +100,116 @@ def probability_dict(n):
 
 
 
+# PMI
+def pmi_score(p_combined, p_1, p_2):
+	denominator = float(p_1 * p_2)
+	fraction = float(p_combined / denominator)
+	return math.log(fraction, 2)
+
+def pmi_score_dict_for_2():
+	# data
+	bigram_dict = probability_dict(2)
+	unary_dict = probability_dict(1)
+	the_pmi_score_dict = dict()
+	# populate the_pmi_score_dict
+	for bigram in bigram_dict:
+		# terms
+		bi_1 = bigram.split()[0]
+		bi_2 = bigram.split()[1]
+		# probabilities
+		p_combined = bigram_dict[bigram]
+		p_1 = unary_dict[bi_1]
+		p_2 = unary_dict[bi_2]
+		# add the pmi to the dictionary
+		the_pmi_score_dict[bigram] = round(float(pmi_score(p_combined, p_1, p_2)),2)
+	# return
+	return the_pmi_score_dict
+
+
+
+# TOP TEN
+def merge_dict(d1, d2):
+
+	# create the return object 
+	d = {}
+
+	# get smallest elements until one dict is empty
+	while len(d1) > 0 and len(d2) > 0:
+
+		# create the traverse objects
+		d1_v, d2_v = next(iter(d1)), next(iter(d2))
+
+		# put the larger score into the return dictionary
+		if d1[d1_v] > d2[d2_v]:
+			d[d1_v] = d1[d1_v]
+			del d1[d1_v]
+		else:
+			d[d2_v] = d2[d2_v]
+			del d2[d2_v]
+
+	# add remaining elements
+	while len(d1) > 0:
+		d1_v = next(iter(d1))
+		d[d1_v] = d1[d1_v]
+		del d1[d1_v]
+	while len(d2) > 0:
+		d2_v = next(iter(d2))
+		d[d2_v] = d2[d2_v]
+		del d2[d2_v]
+
+	# return
+	return d
+
+def merge_sort_dict(d):
+
+	# objects
+	d1 = {}
+	d2 = {}
+	length = len(d)
+	middle = int(length / 2)
+
+	if len(d) > 1:
+		for i in range(0, middle, 1):
+			next_item = next(iter(d))
+			d1[next_item] = d[next_item]
+			del d[next_item]
+
+		for i in range(middle, length, 1):
+			next_item = next(iter(d))
+			d2[next_item] = d[next_item]
+			del d[next_item]
+
+		d1 = merge_sort_dict(d1)
+		d2 = merge_sort_dict(d2)
+		d = merge_dict(d1, d2)
+	
+	return d
+
+def top_ten(d):
+	source_dict = merge_sort_dict(d)
+	if len(source_dict) < 11:
+		return source_dict
+	return_dict = dict()
+	total = 0
+	value = None
+	while total < 11:
+		key = next(iter(source_dict))
+		value = source_dict[key]
+		return_dict[key] = value
+		del source_dict[key]
+		total += 1
+	new_value = value
+	while new_value == value:
+		key = next(iter(source_dict))
+		new_value = source_dict[key]
+		if new_value == value:
+			return_dict[key] = new_value
+			del source_dict[key]
+	return return_dict
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -107,28 +217,20 @@ if __name__ == '__main__':
 	note to self: you're tired, but remember that order matters with ngrams e.g. mad dog vs dog mad
 	"""
 
+	# go to the file
 	go_q2_to_tweets()
-	# file_name_list = get_file_names()
-	# for file_name in file_name_list:
-	# 	file_contents_list = file_to_list(file_name)
-	# 	file_ngrams = list_to_ngram_list(file_contents_list, 1)
-	# 	print(file_contents_list)
-	# 	print(file_ngrams)
-	# 	print("")
 
-	print(all_terms(1))
+	# pmi scores
 	print("")
-	print(all_terms(2))
+	print("PMI Scores")
+	print(pmi_score_dict_for_2())
+
+	# pmi scores
 	print("")
-	print(combine_lists(1))
+	print("Sorted PMI Scores")
+	print(merge_sort_dict(pmi_score_dict_for_2()))
+
+	# top ten
 	print("")
-	print(combine_lists(2))
-	print("")
-	print(frequency_dict(1))
-	print("")
-	print(frequency_dict(2))
-	print("")
-	print(probability_dict(1))
-	print("")
-	print(probability_dict(2))
-	print("")
+	print("Top Ten PMI Scores")
+	print(top_ten(pmi_score_dict_for_2()))
