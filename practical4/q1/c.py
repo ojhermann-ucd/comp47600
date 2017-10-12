14dd1024443a72acb1ea732e5a94f0d0ba2fec57
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-12 16:07:10 +0100

    q1 done

--- practical4/q1/c.py
+++ practical4/q1/c.py
@@ -11,3 +11,11 @@ if __name__ == "__main__":
 	print("TD-IDF Score Matrix")
 	test_matrix_2 = q1.tf_idf_matrix()
 	q1.print_matrix(test_matrix_2)
+
+	# WORDCLOUD INPUT
+	print("")
+	print("TF-IDF Score Wordcloud Input")
+	input_dict = q1.get_terms()
+	td_idf_boolean_dict = q1.td_idf_boolean(input_dict, 2)
+	word_cloud_input = q1.word_cloud_input(td_idf_boolean_dict)
+	print(word_cloud_input)
