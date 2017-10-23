5ae28feda657df0ca1b5d65c3204a8f2a733be2c
Otto Hermann <otto.hermann@ucdconnect.ie>
2017-10-23 13:18:28 +0100

    q2 done

--- practical6/q2/q2.py
+++ practical6/q2/q2.py
@@ -60,3 +60,14 @@ if __name__ == '__main__':
 		classifier.show_most_informative_features(5)
 		print("")
 		number_of_last_letters += 1
+
+	"""
+	I extended the given formul to allow for n last letters and compared it to one allowing for n first letters
+	I also modified the code to use all of the data: 50% in training and 50% in test
+	- this could easily be modified to change either the split or volume of total data used
+	I've included a graph of one run of the two metrics
+	- note that given I shuffle the data, each run will be slightly different
+	- however, all runs demonstrate the same behaviour
+	In this data set, using 2 of the last letters is the most accurate predictor, both on an accuracy score of 77% and on finding the highest average odds of individual names e.g. 'na' is 93.5:1 a female name
+	It would be interesting to run this type of analysis, but to stratify the names by geography, culture, or other metrics e.g. I would expect Latin American names with generised suffixes to be highly predictive of gender, but the phenomena would not be predictive with Germanic names
+	"""
